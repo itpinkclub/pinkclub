af6bb624d0e6b02de411e194942ee2fd1a417bfd
It Pink Club <lumagabay@gmail.com>
2026-07-17 14:32:17 -0300

    Corrige export do Firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,223 +1,40 @@
-import { db } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-    collection,
-    addDoc,
-    getDocs,
-    deleteDoc,
-    doc
+import { 
+    getFirestore 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+import { 
+    getAuth 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// CLOUDINARY
-const cloudName = "emyj9k2e";
-const uploadPreset = "itpinkclub";
 
 
-// ELEMENTOS
-const salvar = document.getElementById("salvar");
-const lista = document.getElementById("lista");
+const firebaseConfig = {
 
+  apiKey: "AIzaSyD9ojNdDkM9M6iO16SW8k7wfWnnJbvHS40",
 
+  authDomain: "itpinkclub-a786e.firebaseapp.com",
 
-// SALVAR PRODUTO
-salvar.addEventListener("click", async () => {
+  projectId: "itpinkclub-a786e",
 
-    const nome = document.getElementById("nome").value;
-    const categoria = document.getElementById("categoria").value;
-    const preco = document.getElementById("preco").value;
-    const link = document.getElementById("link").value;
+  storageBucket: "itpinkclub-a786e.firebasestorage.app",
 
-    const imagemArquivo = document.getElementById("imagem").files[0];
+  messagingSenderId: "716111814812",
 
-    const destaque = document.getElementById("destaque").checked;
-    const ativo = document.getElementById("ativo").checked;
+  appId: "1:716111814812:web:06e1c76950d39021698d59",
 
-
-    if(!nome || !categoria || !preco || !link || !imagemArquivo){
-
-        alert("Preencha todos os campos!");
-        return;
-
-    }
-
-
-
-    try {
-
-
-        // ENVIA IMAGEM PARA CLOUDINARY
-
-        const formData = new FormData();
-
-        formData.append(
-            "file",
-            imagemArquivo
-        );
-
-        formData.append(
-            "upload_preset",
-            uploadPreset
-        );
-
-
-
-        const resposta = await fetch(
-            `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-            {
-                method:"POST",
-                body:formData
-            }
-        );
-
-
-        const imagem = await resposta.json();
-
-
-        const urlImagem = imagem.secure_url;
-
-
-
-        // SALVA NO FIRESTORE
-
-        await addDoc(
-            collection(db,"produtos"),
-            {
-
-                nome,
-                categoria,
-                preco:Number(preco),
-                link,
-                imagem:urlImagem,
-                destaque,
-                ativo,
-                criadoEm:new Date()
-
-            }
-        );
-
-
-
-        alert("Produto salvo com sucesso!");
-
-
-
-        limparCampos();
-
-        carregarProdutos();
-
-
-
-    } catch(error){
-
-        console.error(error);
-
-        alert(
-            "Erro ao salvar produto"
-        );
-
-    }
-
-
-
-});
-
-
-
-
-// LISTAR PRODUTOS
-
-async function carregarProdutos(){
-
-
-    lista.innerHTML="";
-
-
-    const produtos = await getDocs(
-        collection(db,"produtos")
-    );
-
-
-    produtos.forEach((item)=>{
-
-
-        const produto = item.data();
-
-
-        lista.innerHTML += `
-
-        <div class="produto">
-
-            <img 
-            src="${produto.imagem}"
-            width="200"
-            >
-
-            <h3>${produto.nome}</h3>
-
-            <p>${produto.categoria}</p>
-
-            <p>
-            R$ ${produto.preco}
-            </p>
-
-
-            <a href="${produto.link}" target="_blank">
-            Comprar
-            </a>
-
-
-            <button 
-            onclick="excluirProduto('${item.id}')">
-            Excluir
-            </button>
-
-
-        </div>
-
-        `;
-
-
-    });
-
-
-}
-
-
-
-// EXCLUIR
-
-window.excluirProduto = async function(id){
-
-
-    await deleteDoc(
-        doc(db,"produtos",id)
-    );
-
-
-    carregarProdutos();
-
+  measurementId: "G-3XSF7CYT1D"
 
 };
 
 
 
-
-// LIMPAR FORM
-
-function limparCampos(){
-
-
-    document.getElementById("nome").value="";
-    document.getElementById("categoria").value="";
-    document.getElementById("preco").value="";
-    document.getElementById("link").value="";
-    document.getElementById("imagem").value="";
-
-}
+const app = initializeApp(firebaseConfig);
 
 
 
-// iniciar lista
+export const db = getFirestore(app);
 
-carregarProdutos();
+
+export const auth = getAuth(app);
